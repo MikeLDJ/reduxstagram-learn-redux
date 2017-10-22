b7efec032eb70220925ab6498af618577e2e20b3
MikeLDJ <mikeu76@gmail.com>
2017-10-22 19:19:13 +0200

    lesson 7 finished

---
+++ learn-redux/client/actions/actionCreators.js
@@ -0,0 +1,23 @@
+function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+  }
+}
+
+function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+
+function removeComment(postId, i) {
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
+  }
+}
