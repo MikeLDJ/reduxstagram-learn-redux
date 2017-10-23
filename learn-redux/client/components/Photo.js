42ce56e5cd6a2ff899081ab91881c98f9700d87b
MikeLDJ <mikeu76@gmail.com>
2017-10-23 21:20:03 +0200

    finished lesson 16 - end

--- learn-redux/client/components/Photo.js
+++ learn-redux/client/components/Photo.js
@@ -12,12 +12,8 @@ const Photo = React.createClass({
             <img src={post.display_src} alt={post.caption} className="grid-photo" />
           </Link>
 
-          <CSSTransitionGroup transitionName="like"
-            transitionEnterTimeout={500}
-            transitionLeaveTimeout={500}>
-            <span key={post.likes} className="likes-heart">
-              {post.likes}
-            </span>
+          <CSSTransitionGroup transitionName="like" transitionEnterTimeout={500} transitionLeaveTimeout={500}>
+            <span key={post.likes} className="likes-heart">{post.likes}</span>
           </CSSTransitionGroup>
 
         </div>
@@ -25,19 +21,16 @@ const Photo = React.createClass({
         <figcaption>
           <p>{post.caption}</p>
           <div className="control-buttons">
-            <button
-              onClick={this.props.increment.bind(null, i)}
-              className="likes">
-              &hearts; {post.likes}
-            </button>
+            <button onClick={this.props.increment.bind(null, i)} className="likes">&hearts; {post.likes}</button>
             <Link className="button" to={`/view/${post.code}`}>
               <span className="comment-count">
                 <span className="speech-bubble"></span>
-                {comments[post.code] ? comments[post.code].length : 0}
+                {comments[post.code] ? comments[post.code].length : 0 }
               </span>
             </Link>
           </div>
         </figcaption>
+
       </figure>
     )
   }
